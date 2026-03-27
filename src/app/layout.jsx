cf874c080a7d453141e485cddd9b7490cee5ff0b
Akash Rathore <akash.rathore@nuagebiz.tech>
2026-03-27 12:41:09 +0530

    analytics added

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,4 +1,5 @@
 import "@/app/globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata = {
   title: "Resume Analyzer",
@@ -8,7 +9,10 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        {children}
+        <Analytics />
+      </body>
     </html>
   );
 }
